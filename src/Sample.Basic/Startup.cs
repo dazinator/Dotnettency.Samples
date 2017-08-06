e4f243985ceb90e5175c344a8f828a9ccea3dcca
Darrell Tunnell <darrell.tunnell@googlemail.com>
2017-08-06 13:02:52 +0100

    Intial Commit

---
+++ src/Sample.Basic/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Dotnettency;
+using System.Threading.Tasks;
+
+namespace Sample.Basic
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMultiTenancy<Tenant>((multiTenancyOptions) =>
+            {
+                multiTenancyOptions
+                    .InitialiseTenant<TenantShellFactory>();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMultitenancy<Tenant>((options) =>
+            {
+
+            });
+
+            app.Run(async (context) =>
+            {
+                var tenantTask = context.RequestServices.GetRequiredService<Task<Tenant>>();
+                var tenant = await tenantTask;
+
+                await context.Response.WriteAsync("Hello: " + tenant.Name);
+            });
+
+        }
+    }
+}
---
+++ src/Sample.Basic/Tenant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample.Basic
+{
+    public class Tenant
+    {
+        public Tenant(Guid tenantGuid, string name)
+        {
+            TenantGuid = tenantGuid;
+            Name = name;
+        }
+
+        public Guid TenantGuid { get; set; }
+        public string Name { get; set; }
+    }
+}
