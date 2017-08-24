b0edfd901e5ee8868e76ca547231b6f340eadb4a
Darrell Tunnell <darrell.tunnell@googlemail.com>
2017-08-24 22:48:07 +0100

    Added new sample for tenant containers.

---
+++ src/Sample.TenantContainer/GreetingService.cs
@@ -0,0 +1,25 @@
+﻿namespace Sample.TenantContainer
+{
+    public class GreetingService
+    {
+        private readonly bool _isVisitorWelcome;
+
+        public GreetingService(bool isVisitorWelcome)
+        {
+            _isVisitorWelcome = isVisitorWelcome;
+        }
+
+        public string Greet()
+        {
+            if (_isVisitorWelcome)
+            {
+                return "WILL YOU GO AWAY ALREADY!!";
+            }
+            else
+            {
+                return "Pleased to meet you.";
+            }
+        }
+
+    }
+}
---
+++ src/Sample.TenantContainer/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Dotnettency;
+using System;
+
+namespace Sample.TenantContainer
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            return services.AddMultiTenancy<Tenant>((multiTenancyOptions) =>
+            {
+                multiTenancyOptions
+                    .InitialiseTenant<TenantShellFactory>()
+                    .ConfigureTenantContainers((containerBuilder) =>
+                    {
+                        containerBuilder.WithStructureMap((tenant, tenantServices) =>
+                        {
+                            if (tenant.Name == "Moogle")
+                            {
+                                tenantServices.AddSingleton<GreetingService>(new GreetingService(true));
+                            }
+                            else
+                            {
+                                tenantServices.AddSingleton<GreetingService>(new GreetingService(false));
+                            }
+                        });
+                    });
+            });
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseMultitenancy<Tenant>((options) =>
+            {
+                options.UsePerTenantContainers();
+            });
+
+            app.Run(async (context) =>
+            {
+                var greetingService = context.RequestServices.GetRequiredService<GreetingService>();
+                await context.Response.WriteAsync(greetingService.Greet());
+            });
+
+        }
+    }
+}
